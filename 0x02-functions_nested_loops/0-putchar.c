404c339cb2e171e8a5a1f81e8e48fef9b6dd0e83
yinkuzzy05 <olayinkaoluwarotimi93@gmail.com>
2022-09-13 10:29:16 -0700

    trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-#include "_putchar.h"
+#include "holberton.h"
 
 /**
- * main - prints _putchar
+ * main - prints Holberton
  *
- * Description: prints _putchar
+ * Description: prints Holberton
  *
  * Return: 0 (Success)
  */
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-learning
+still learning
