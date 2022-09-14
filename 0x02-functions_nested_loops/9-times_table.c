f1fb0c3a8ff39942308302b6ea1713d63c074e33
yinkuzzy05 <olayinkaoluwarotimi93@gmail.com>
2022-09-14 00:30:04 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 
 	for (num = 0; num <= 0; num++)
 	{
-		_puthar('0');
+		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
@@ -23,7 +23,7 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + '0');
 
-			_putchar((prod % 10) + '0");
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
