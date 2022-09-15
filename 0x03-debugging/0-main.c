5b348a21fdc72d4a16d4c99b6c82663d4c714ffe
yinkuzzy05 <olayinkaoluwarotimi93@gmail.com>
2022-09-15 13:54:11 -0700

    attempt'

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,15 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Rteurn: 0
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
-	i = 98:
-	positive_or_negative(i);
+	i = 98;
+	positive_or_negativve(1);
 
 	return (0);
 }
-
