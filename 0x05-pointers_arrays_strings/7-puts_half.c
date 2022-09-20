50c3fca26ba23e8e6bd3756b5c4231350094ab5a
yinkuzzy05 <olayinkaoluwarotimi93@gmail.com>
2022-09-20 15:23:45 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * puts_half - puts half
@@ -19,6 +20,6 @@ void puts_half(char *str)
 	else
 		y = x / 2;
 	for (i = y; i < x; i++)
-		_putchar(str[i]);
+		putchar(str[i]);
 	_putchar('\n');
 }
