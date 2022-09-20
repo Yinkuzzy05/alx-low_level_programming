8377ce81c54abbedd2e87a5146b5fbec6c40933a
yinkuzzy05 <olayinkaoluwarotimi93@gmail.com>
2022-09-20 05:58:12 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - prints a string in stdout
+ * puts2 - prints every other character of a  string to stdout
  * @str: string to print
  *
  * Return: void
  */
-void _puts(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
 	while (*(str + i))
 	{
 		_putchar(*(str + i));
-		i++;
+		i = i + 2;
 	}
 	_putchar('\n');
 }
