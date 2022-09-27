b298f8ce65ae6136b620d3d28abc8c47b55200d9
yinkuzzy05 <olayinkoluwarotimi93@gmail.com>
2022-09-27 08:26:15 -0700

    task 2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _memcpy - Copies memory area
  *
  * @dest: where to copy the memory to
- * @n: the number of bytes
+ * @n: the number of buffer
  * @src: the memory area to copy from
  *
- * Return: a pointer to @dest
+ * Return: a new string
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
