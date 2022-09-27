101c0e61b0fc0080d7640c446e8d2a931ad8c5a4
yinkuzzy05 <olayinkoluwarotimi93@gmail.com>
2022-09-27 04:58:41 -0700

    yh

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (i < n)
 	{
-		s[i] = b;
+		*(s + i) = b;
+		i++;
 	}
 
 	return (s);
