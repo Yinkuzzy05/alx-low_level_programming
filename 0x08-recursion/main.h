16aad92285def93a14bb90c6e483800d2dfca3c2
yinkuzzy05 <olayinkoluwarotimi93@gmail.com>
2022-09-28 02:03:16 -0700

    task 7

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototype()
+ * int prototype()
+ */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,3 +18,5 @@ int is_palindrome(char *s);
 int helper(int a, int b);
 int primeFinder(int a, int b);
 int palindromeChecker(char *str, int len, int i);
+
+#endif /*MAIN_H*/
