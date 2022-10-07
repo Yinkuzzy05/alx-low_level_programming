2ca0ea556e596b243d912de034ecc9d8392e2247
yinkuzzy05 <olayinkaoluwarotimi93@gmail.com>
2022-10-07 09:23:27 -0700

    task 3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,4 +22,23 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 	if (ptr == NULL)
+	{
+		ar = malloc(new_size);
+		return (ar);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
+	ar = malloc(new_size);
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		ar[i] = oldar[i];
+
+	free(oldar);
+	return (ar);
+}
